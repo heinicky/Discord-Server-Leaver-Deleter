ee66c0d8cfd6f1369f6b1f95f11b4a7aa4485c0e
nick <97973680+heinicky@users.noreply.github.com>
2022-10-05 05:17:53 -0500

    Update console.py

--- console.py
+++ console.py
@@ -2,15 +2,6 @@ from colorama import Fore, init
 import os
 
 class Console:
-    ##the reason i created an instanciation is because windows has a bug where colorama will NOT work if the console isn't cleared.
-    def __init__(self) -> None:
-        if os.name == 'nt':
-            print('Windows detected. due to windows being gay with colorama, the console has to be cleared.')
-            os.system('cls')
-            init()
-        else:
-            pass
-
     def print_logo(self, username):
         print(f'''{Fore.CYAN}
         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
